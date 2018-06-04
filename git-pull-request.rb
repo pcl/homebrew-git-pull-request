ff2f6cddd15f390d3468bc3ae25cf3a968a69dc2
Patrick Linskey <plinskey@cisco.com>
2018-06-04 20:29:22 +0200

    bump to v0.2

--- git-pull-request.rb
+++ git-pull-request.rb
@@ -1,8 +1,8 @@
 class GitPullRequest < Formula
   desc "Manage GitHub pull requests from the shell"
   homepage "https://github.com/pcl/git-pull-request"
-  url "https://github.com/pcl/git-pull-request/archive/v0.1.tar.gz"
-  sha256 "3c1adc218fd6c971ef4822825dad882158b60ffed71e392e551d98ef46657d39"
+  url "https://github.com/pcl/git-pull-request/archive/v0.2.tar.gz"
+  sha256 "151dbae818e30d1acae6cfd6689a0ea468d3137000d829a6a4a3e6236160a29c"
 
   depends_on "python@3" => :required
 
